f196e2fb3fefa478ea11f6240e4dee188a234307
retracement <mark.broadbent@sqlcambs.org.uk>
2020-01-29 20:38:15 +0000

    updated files

---
+++ Demo/2b Correctness session 2.sql
@@ -0,0 +1,201 @@
+/*************************************************************/
+/* All scripts contained within are Copyright © 2015 of      */
+/* Mark Broadbent, whether they are derived or actual        */
+/* works by him or his representatives                       */
+/*************************************************************/
+/* They are distributed under the Apache 2.0 licence and any */
+/* reproducion, transmittion, storage, or derivation must    */
+/* comply with the terms under the licence linked below.     */
+/* If in any doubt, contact the license owner for written    */
+/* permission by emailing contactme@sturmovik.net            */
+/*************************************************************
+Copyright [2019] [Mark Broadbent]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
+/**********************************/
+/* Consistency and bad behaviours */
+/**********************************/
+-- In this section we will demonstrate how SQL Server
+-- can return inconsistent or problematic result
+
+
+--------------------------------------------------------
+-- In this example we will look at inconsistent reads --
+--------------------------------------------------------
+
+-- Run a while loop to insert all visible records to a temp table and BREAK
+-- if record count is not equal to 10.
+-- Excecute this several times until you are happy
+USE [2Fast2Furious]
+GO
+IF @@TRANCOUNT <> 0 ROLLBACK
+SET NOCOUNT ON
+DECLARE @Cars TABLE (id uniqueidentifier DEFAULT NEWID(), carname VARCHAR(20), 
+	lastservice datetime DEFAULT getdate(), SpeedMPH INT, Details CHAR (7000));
+DECLARE @ConsistentResults INT = 0
+WHILE 1=1
+BEGIN
+	DELETE FROM @Cars
+	INSERT INTO @Cars SELECT * FROM Cars --with (holdlock) --with (serializable)
+	IF @@ROWCOUNT <> 10
+		BREAK
+
+	SET @ConsistentResults = @ConsistentResults + 1
+	WAITFOR DELAY '00:00:00.013'
+END
+SELECT @ConsistentResults AS SuccessfulPriorRuns
+SELECT * FROM @Cars
+
+-- Note the biggest trigger for this problem is the table Cluster GUID
+-- and the way SQL accesses mixed extents
+-- See https://www.sqlskills.com/blogs/paul/read-committed-doesnt-guarantee-much/
+
+
+
+-- Let's repeat under SERIALIZABLE
+-- Excecute this several times until you are happy
+USE [2Fast2Furious]
+GO
+IF @@TRANCOUNT <> 0 ROLLBACK
+SET NOCOUNT ON
+DECLARE @Cars TABLE (id uniqueidentifier DEFAULT NEWID(), carname VARCHAR(20), 
+	lastservice datetime DEFAULT getdate(), SpeedMPH INT, Details CHAR (7000));
+DECLARE @ConsistentResults INT = 0
+WHILE 1=1
+BEGIN
+	BEGIN TRY
+		DELETE FROM @Cars
+		INSERT INTO @Cars SELECT * FROM Cars WITH (SERIALIZABLE)
+		IF @@ROWCOUNT <> 10
+			BREAK
+
+		SET @ConsistentResults = @ConsistentResults + 1
+		WAITFOR DELAY '00:00:00.013'
+	END TRY
+	BEGIN CATCH -- we need catch in this scenario to output runs on DL
+		SELECT @ConsistentResults AS SuccessfulPriorRuns
+		SELECT * FROM @Cars
+		SELECT ERROR_MESSAGE() 
+		BREAK
+	END CATCH
+END
+
+
+-- Let's transition to a new OPTIMISTIC default
+-- of READ_COMMITTED_SNAPSHOT
+USE MASTER
+GO
+ALTER DATABASE [2Fast2Furious] SET READ_COMMITTED_SNAPSHOT ON WITH ROLLBACK IMMEDIATE;
+GO
+USE [2Fast2Furious];
+GO
+
+
+-- Run again using the new default of READ_COMMITTED_SNAPSHOT
+USE [2Fast2Furious]
+GO
+IF @@TRANCOUNT <> 0 ROLLBACK
+SET NOCOUNT ON
+DECLARE @Cars TABLE (id uniqueidentifier DEFAULT NEWID(), carname VARCHAR(20), 
+	lastservice datetime DEFAULT getdate(), SpeedMPH INT, Details CHAR (7000));
+DECLARE @ConsistentResults INT = 0
+WHILE 1=1
+BEGIN
+	DELETE FROM @Cars
+	INSERT INTO @Cars SELECT * FROM Cars --with (holdlock) --with (serializable)
+	IF @@ROWCOUNT <> 10
+		BREAK
+
+	SET @ConsistentResults = @ConsistentResults + 1
+	WAITFOR DELAY '00:00:00.013'
+END
+SELECT @ConsistentResults AS SuccessfulPriorRuns
+SELECT * FROM @Cars
+
+
+-- Finally let's repeat under SNAPSHOT
+-- and cancel once you are happy
+SET TRANSACTION ISOLATION LEVEL SNAPSHOT
+USE [2Fast2Furious]
+GO
+IF @@TRANCOUNT <> 0 ROLLBACK
+SET NOCOUNT ON
+DECLARE @Cars TABLE (id uniqueidentifier DEFAULT NEWID(), carname VARCHAR(20), 
+	lastservice datetime DEFAULT getdate(), SpeedMPH INT, Details CHAR (7000));
+DECLARE @ConsistentResults INT = 0
+WHILE 1=1
+BEGIN
+	DELETE FROM @Cars
+	INSERT INTO @Cars SELECT * FROM Cars --SNAPSHOT HINT ONLY RELEVANT TO IMTABLES
+	IF @@ROWCOUNT <> 10
+		BREAK
+
+	SET @ConsistentResults = @ConsistentResults + 1
+	WAITFOR DELAY '00:00:00.013'
+END
+
+
+-- set isolation level back to read committed
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+
+
+-- Let's transition back to READ COMMITTED default
+USE MASTER
+GO
+ALTER DATABASE [2Fast2Furious] SET READ_COMMITTED_SNAPSHOT OFF WITH ROLLBACK IMMEDIATE;
+GO
+USE [2Fast2Furious];
+GO
+
+
+-- Switch back to session 1
+-- and stop the while loop if still running
+-- or reconnect session
+
+
+
+--------------------------------------------------------
+-- In this example we will look at atomicity concerns --
+--------------------------------------------------------
+-- and attempt to solve them
+SET NOCOUNT OFF
+USE [2Fast2Furious]
+GO
+IF @@TRANCOUNT <> 0 ROLLBACK
+
+
+-- Set a lock timeout to avoid any waiting on lock
+SET LOCK_TIMEOUT 10
+
+
+-- Run a transaction to insert a new record
+-- and DELETE any records with an ID of 1
+BEGIN TRAN
+    INSERT INTO t1 VALUES ('2');
+    DELETE FROM t1 WHERE c1 = 1;
+COMMIT
+
+
+-- Check transaction has rolled back (or committed...)
+SELECT @@TRANCOUNT AS Trancount
+
+
+-- Query table and skip over other
+-- session locked records (if any)
+SELECT * FROM t1 WITH (READPAST);
+
+-- Errm!
+
+
+-- Switch back to session 1
---
+++ Demo/5b IM Concurrency session 2.sql
@@ -0,0 +1,114 @@
+/*************************************************************/
+/* All scripts contained within are Copyright © 2015 of      */
+/* Mark Broadbent, whether they are derived or actual        */
+/* works by him or his representatives                       */
+/*************************************************************/
+/* They are distributed under the Apache 2.0 licence and any */
+/* reproducion, transmittion, storage, or derivation must    */
+/* comply with the terms under the licence linked below.     */
+/* If in any doubt, contact the license owner for written    */
+/* permission by emailing contactme@sturmovik.net            */
+/*************************************************************
+Copyright [2019] [Mark Broadbent]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
+/*******************************************/
+/* Concurrency with In-Memory transactions */
+/*******************************************/
+-- In this section we will demonstrate how SQL Server
+-- handles isolation with in-memory transactions
+
+
+--------------------------------------------------------
+-- In this example we will look at IMOLTP concurrency --
+--------------------------------------------------------
+-- We will see how IMOLTP enables concurrency
+
+
+-- One record has been updated in the table
+-- so on-disk read committed should block......
+-- query all table records under read committed
+SET NOCOUNT ON
+USE [2Fast2Furious]
+GO
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+SELECT * FROM [dbo].[ArrestsIM] ORDER BY id
+-- Check out id 6 that is being updated in an open ended transaction
+
+-- lets repeat under serializable
+SET NOCOUNT ON
+USE [2Fast2Furious]
+GO
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+SELECT * FROM [dbo].[ArrestsIM] ORDER BY id
+/*
+Msg 41333, Level 16, State 1, Line 37
+The following transactions must access memory optimized tables and natively compiled modules under 
+snapshot isolation: RepeatableRead transactions, Serializable transactions, and transactions that 
+access tables that are not memory optimized in RepeatableRead or Serializable isolation.
+*/
+
+
+-- So lets attempt to create Native Compilation Stored Procedure
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+USE [2Fast2Furious]
+GO
+
+CREATE PROCEDURE dbo.QueryArrestsIM
+	WITH NATIVE_COMPILATION,
+	SCHEMABINDING,
+	EXECUTE AS OWNER
+AS
+	BEGIN ATOMIC WITH -- Create tran if no open or create savepoint
+		(TRANSACTION ISOLATION LEVEL = SERIALIZABLE,
+		LANGUAGE = N'british' -- language required
+		)
+		BEGIN
+			SELECT id, Arrest_Date, Charges, Details FROM [dbo].[ArrestsIM]
+				ORDER BY id
+		END
+	END
+GO
+
+-- Note the error :)
+
+
+-- Try again
+CREATE PROCEDURE dbo.QueryArrestsIM
+	WITH NATIVE_COMPILATION,
+	SCHEMABINDING,
+	EXECUTE AS OWNER
+AS
+	BEGIN ATOMIC WITH -- Create tran if no open or create savepoint
+		(TRANSACTION ISOLATION LEVEL = SERIALIZABLE, -- SNAPSHOT or REPEATABLE READ
+		LANGUAGE = N'british' -- language required
+		)
+		BEGIN
+			SELECT id, Arrest_Date, Charges, Details FROM [dbo].[ArrestsIM]
+				ORDER BY id
+		END
+	END
+GO
+
+
+-- Execute Native Compilation Stored Procedure
+EXEC dbo.QueryArrestsIM
+
+-- Did SERIALIZABLE for In-Memory have to wait?
+
+
+-- Switch back to session 1
+
+
+--fin
